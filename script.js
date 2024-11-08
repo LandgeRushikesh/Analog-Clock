462c1e81049e2c5e6e1d71f553d17bd7486387dd
LandgeRushikesh <rushilandge12@gmail.com>
2024-11-09 00:43:41 +0530

    bug fixed

---
+++ script.js
@@ -0,0 +1,17 @@
+let hour = document.querySelector(".hour")
+let Min = document.querySelector(".min")
+let Sec = document.querySelector(".sec")
+
+setInterval(()=>{
+    let date = new Date()
+    let hrs = date.getHours()
+    let min = date.getMinutes()
+    let sec = date.getSeconds()
+    rotate(hrs,min,sec)
+},1000)
+
+let rotate = (hrs,min,sec)=>{
+    hour.style.transform = `rotate(${30*hrs+0.5*min})deg)`
+    Min.style.transform = `rotate(${6*min}deg)`
+    Sec.style.transform = `rotate(${6*sec}deg)`
+}
